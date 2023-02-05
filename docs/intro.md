df112923c165812fc743a42b07962d7607a83f47
Mackan <52716811+VpgMackan@users.noreply.github.com>
2023-02-05 22:29:56 +0100

    Initila commit

---
+++ docs/intro.md
@@ -0,0 +1,32 @@
+---
+sidebar_position: 1
+---
+
+# Tutorial Intro
+
+Let's discover **Budget Cat in less than 5 minutes**.
+
+## Getting Started
+
+Get started by **going to the [Budget Cat Website](https://budgetcat.me)**.
+And click on the **Get Started** button to create your account.
+
+
+### What you'll need
+
+- Email address
+- Internet connection
+- A computer or mobile device
+
+That is all you need to get started.
+
+## Creating your account
+
+Click on the **Get Started** button on the [Budget Cat Website](https://budgetcat.me).
+
+Enter your email address and click on the **Continue** button. Then enter the password you want to use and click on the **Continue** button again.
+Once you have done that, you will need to enter your username and click on the **Finish** button.
+
+## Logging in
+
+Once you have created your account, you can log in by going to the [Budget App Website](https://app.budgetcat.me) and clicking on the **Login** button.
---
+++ docusaurus.config.js
@@ -0,0 +1,121 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Budgetcat',
+  tagline: 'Budgetcat is a personal finance app that helps you to manage your money and save money.',
+  favicon: 'img/favicon.png',
+
+  url: 'https://docs.budgetcat.me',
+  baseUrl: '/',
+
+  organizationName: 'Budgetcat',
+  projectName: 'Documentation',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/budgetcat/documentation/',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl:
+            'https://github.con/budgetcat/documentation/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Budgetcat',
+        logo: {
+          alt: 'Budgetcat Logo',
+          src: 'img/favicon.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Documentation',
+          },
+          { to: '/blog', label: 'Blog', position: 'left' },
+          {
+            href: 'https://github.com/budgetcat/documentation',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'TikTok',
+                href: 'https://www.tiktok.com/@budgetcatapp',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/budgetcatapp',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/budgetcat',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Budget Cat, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
